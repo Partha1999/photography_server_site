fc41ff7ce885ff110a89692b1ea74e186b790be2
Partha1999 <impartha1999@gmail.com>
2022-12-22 21:51:06 +0600

    another update

--- index.js
+++ index.js
@@ -32,12 +32,7 @@ async function run() {
             res.send(result);
           });
 
-    app.get("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const service = await serviceCollection.findOne(query);
-      res.send(service);
-    });
+    
 
     //reviews area
 
