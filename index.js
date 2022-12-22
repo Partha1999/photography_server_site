f82fcfb12a8429d2d1419eb650b3c8fa348b06bb
Partha1999 <impartha1999@gmail.com>
2022-12-22 21:53:50 +0600

    token2

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ async function run() {
       res.send(service);
     });
 
-   
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
     app.post("/reviews", async (req, res) => {
       const review = req.body;
